f58bfcb1b5cf2d37a83b7a8a34a428eb38848910
FabianINC <fabiguzmang1205@gmail.com>
2023-01-05 16:21:35 -0600

    Actualizacion

--- app/src/main/java/MainActivity/Main.java
+++ app/src/main/java/MainActivity/Main.java
@@ -3,6 +3,8 @@ package MainActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.archivo.move.R;
 import com.archivo.move.databinding.SideBarBinding;
@@ -31,6 +33,9 @@ public class Main extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 
 
